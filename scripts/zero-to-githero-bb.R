9063d85d040f5ca5ab5bff6feea4bf5324e79182
Melanie Murphy <melanie.murphy@msi-inc.com>
2025-01-07 13:01:36 -0500

    initial

---
+++ scripts/zero-to-githero-bb.R
@@ -0,0 +1,26 @@
+# Group 1 ----
+
+# If you had to create a team name based on your group members' initials, 
+# what would it be?
+# Response: 
+
+
+
+
+# Group 2 ---- 
+
+# If you had to create a team name based on your group members' initials, 
+# what would it be?
+# Response: 
+
+
+
+
+# Group 3 ----
+
+# If you had to create a team name based on your group members' initials, 
+# what would it be?
+# Response: 
+
+
+
